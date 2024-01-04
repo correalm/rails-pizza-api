794965ced00d643e580c86f48ca919e2c7254ac5
correalm <luismigueldasilva.99@gmail.com>
2024-01-04 13:36:14 -0300

    feat(models/pizza): validate presence of name and price

--- spec/models/pizza_spec.rb
+++ spec/models/pizza_spec.rb
@@ -2,4 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Pizza, type: :model do
   it { should have_many(:order_items) }
+
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:price) }
 end
