4ab5ffeffbdb2b1210666112436db09f7240287f
correalm <luismigueldasilva.99@gmail.com>
2024-01-04 09:15:08 -0300

    fix: access body on pizza/request spec

--- spec/requests/pizzas_spec.rb
+++ spec/requests/pizzas_spec.rb
@@ -7,8 +7,11 @@ RSpec.describe 'Pizzas', type: :request do
 
       get pizzas_path
 
+      body = JSON.parse(response.body)
+
       expect(response).to have_http_status :ok
-      expect(response).to match_json_schema('pizzas')
+      expect(body.first).to match_json_schema('pizzas')
+      expect(body.last).to match_json_schema('pizzas')
     end
   end
 end
