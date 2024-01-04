69fed004489acfcad8027832782501bcf49e8886
correalm <luismigueldasilva.99@gmail.com>
2024-01-04 14:02:25 -0300

    spec(requests/pizzas): post method

--- spec/requests/api/pizzas_spec.rb
+++ spec/requests/api/pizzas_spec.rb
@@ -14,4 +14,34 @@ RSpec.describe 'Api::Pizzas', type: :request do
       expect(body.last).to match_json_schema('pizzas')
     end
   end
+
+  describe 'POST /index' do
+    context 'given invalid params' do
+      it 'respond with 422 status code' do
+        params = { pizza: { price: 12.23 } }
+
+        post api_pizzas_path, params: params
+
+        expect(response).to have_http_status :unprocessable_entity
+      end
+    end
+
+    context 'given valid params' do
+      it 'returns 201 status code' do
+        params = { pizza: { price: 12.24, name: 'foo-bar' } }
+
+        post api_pizzas_path, params: params
+
+        expect(response).to have_http_status :created
+      end
+
+      it 'returns the created pizza' do
+        params = { pizza: { price: 12.24, name: 'foo-bar' } }
+
+        post api_pizzas_path, params: params
+
+        expect(response.body).to match_json_schema('pizzas')
+      end
+    end
+  end
 end
