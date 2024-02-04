4523428ea3bbb02af64d887a7b7acc58ebfc7f22
correalm <luismigueldasilva.99@gmail.com>
2024-02-04 11:31:19 -0300

    feat(order_item): validater numericality of quantity

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,6 @@
 class OrderItem < ApplicationRecord
   belongs_to :pizza
   belongs_to :order
+
+  validates :quantity, numericality: true
 end
--- spec/models/order_item_spec.rb
+++ spec/models/order_item_spec.rb
@@ -3,4 +3,6 @@ require 'rails_helper'
 RSpec.describe OrderItem, type: :model do
   it { should belong_to(:pizza) }
   it { should belong_to(:order) }
+
+  it { should validate_numericality_of(:quantity) }
 end
